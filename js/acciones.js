99e9898eb31b04ffebc16cb7de346ae08c4ce2bf
baezalonso <cesar3f.dsaupoo@gmail.com>
2016-05-25 13:56:35 -0500

    5subida

--- js/acciones.js
+++ js/acciones.js
@@ -5,13 +5,13 @@ $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	audio= window.plugins.LowLatencyAudio;
 	
-	audio.preloadFX('do', 'audio/C.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-	audio.preloadFX('re', 'audio/D.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-	audio.preloadFX('mi', 'audio/E.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-	audio.preloadFX('fa', 'audio/F.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-	audio.preloadFX('sol', 'audio/G.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-	audio.preloadFX('la', 'audio/A.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
-    audio.preloadFX('si', 'audio/B.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+	audio.preloadSound('do', 'audio/C.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+	audio.preloadSound('re', 'audio/D.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+	audio.preloadSound('mi', 'audio/E.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+	audio.preloadSound('fa', 'audio/F.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+	audio.preloadSound('sol', 'audio/G.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+	audio.preloadSound('la', 'audio/A.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
+    audio.preloadSound('si', 'audio/B.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg); });
 	
 	$('.nota').bind('touchstart', function() {
 		$(this).addClass('tocada');
